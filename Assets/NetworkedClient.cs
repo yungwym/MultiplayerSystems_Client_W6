4a784dcdbfab9e6730f12107a89f4039ff1c5af9
yungwym <wymer1120@gmail.com>
2021-12-05 20:09:53 -0500

    Commit - Before V2 Refactor

--- Assets/NetworkedClient.cs
+++ Assets/NetworkedClient.cs
@@ -89,7 +89,7 @@ public class NetworkedClient : MonoBehaviour
             hostID = NetworkTransport.AddHost(topology, 0);
             Debug.Log("Socket open.  Host ID = " + hostID);
 
-            connectionID = NetworkTransport.Connect(hostID, "192.168.86.119", socketPort, 0, out error); // server is local on network
+            connectionID = NetworkTransport.Connect(hostID, "192.168.86.138", socketPort, 0, out error); // server is local on network
 
             if (error == 0)
             {
@@ -130,7 +130,7 @@ public class NetworkedClient : MonoBehaviour
         }
         else if (signifier == ServerToClientSignifiers.GameStart)
         {
-            gameSystemManager.GetComponent<SystemManager>().ChangeState(GameStates.Game)
+            gameSystemManager.GetComponent<SystemManager>().ChangeState(GameStates.Game);
         }
         else if (signifier == ServerToClientSignifiers.OpponentPlayed)
         {
